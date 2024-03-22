499b16117adba7e4b8016bea0480c1bf6fb41fae
Beyza Nur Karakaya <beyzaanur.karakaya@gmail.com>
2024-03-22 22:51:27 +0300

    hata düzeltme

--- frontend/src/components/auctions_page/Auctions.jsx
+++ frontend/src/components/auctions_page/Auctions.jsx
@@ -9,13 +9,10 @@ import Pagination from "../general/Pagination";
 
 export default function Auctions() {
   const [products, setProducts] = useState([]);
-<<<<<<< Updated upstream
+
   const [currentPage, setCurrentPage] = useState(1)
   const [postsPerPage, setPostsPerPage] = useState(12);
-=======
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(1);
->>>>>>> Stashed changes
+
   const [isLoading, setIsLoading] = useState(true);
   const token = localStorage.getItem("userToken");
 
@@ -35,36 +32,24 @@ export default function Auctions() {
     console.log(localStorage.getItem("userToken"));
     fetchProducts(); // Call the function
   }, []);
-<<<<<<< Updated upstream
+
   
 	const lastPostIndex = currentPage * postsPerPage;
 	const firstPostIndex = lastPostIndex - postsPerPage;
 	const currentPosts = products.slice(firstPostIndex, lastPostIndex);
 	const totalPages = Math.ceil(products.length/postsPerPage)
-=======
->>>>>>> Stashed changes
-
-  const lastPostIndex = currentPage * postsPerPage;
-  const firstPostIndex = lastPostIndex - postsPerPage;
-  const currentPosts = products.slice(firstPostIndex, lastPostIndex);
-  const totalPages = products.length / postsPerPage;
-
-  if (products.length == 0)
-    return (
-      <>
-        <Navbar />
-        <div>
-          <h3>Gosterilecek bir urun yok.</h3>
-        </div>
-      </>
-    );
 
   return (
     <div>
-<<<<<<< Updated upstream
     	<Container>
 	        <Navbar name="Açık Arttırma Sayfası" />
-	
+			
+			{products.length == 0 && (
+	          <div className="text-light">
+	            Getirilecek bir açık artırma bulunmamaktadır.
+	          </div>
+	        )}
+			
 	        <Link to="/" className="text-white">
 	          {!token && <div>Ana sayfaya dön</div>}
 	        </Link>
@@ -103,41 +88,6 @@ export default function Auctions() {
 	    postsPerPage={postsPerPage}
 	    setCurrentPage={setCurrentPage}
 	    />
-   
-=======
-      <Container>
-        <Navbar name="Açık Arttırma Sayfası" />
-
-        <Link to="/" className="text-white">
-          {!token && <div>Ana sayfaya dön</div>}
-        </Link>
-        {isLoading ? (
-          <p className="text-light">Ürünler yüklenirken lütfen bekleyin.</p>
-        ) : (
-          <Row className="my-3">
-            {currentPosts.map((product, index) => {
-              return (
-                <AuctionCard
-                  key={index}
-                  photoPath={product.photoPath}
-                  productTitle={product.productTitle}
-                  price={product.price}
-                  id={product.id}
-                />
-              );
-            })}
-          </Row>
-        )}
-      </Container>
-      <label className="text-light">
-        Sayfa {currentPage} / {totalPages}
-      </label>
-      <Pagination
-        totalPosts={products.length}
-        postsPerPage={postsPerPage}
-        setCurrentPage={setCurrentPage}
-      />
->>>>>>> Stashed changes
     </div>
   );
 }
