3f7a5a7e4c2fa2a1957bbaf290990c8b668c3d1b
EMİRHAN KARAKOÇ <emirhankarakoc@yahoo.com>
2024-03-24 21:24:02 +0300

    Update MailManager.java

--- backend/src/main/java/com/karakoc/mezat/config/mailservice/MailManager.java
+++ backend/src/main/java/com/karakoc/mezat/config/mailservice/MailManager.java
@@ -1,42 +1,42 @@
 package com.karakoc.mezat.config.mailservice;
 
-
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
-
-public class MailManager implements MailService{
-    private  JavaMailSender mailSender;
+public class MailManager implements MailService {
+    private JavaMailSender mailSender;
 
     @Autowired
     public MailManager(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
 
-
     @Override
     public String sendMail(String to, String icerik) {
-        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
 
-        simpleMailMessage.setFrom("shopifyemirhan6@gmail.com");
-        simpleMailMessage.setTo(to);
-        simpleMailMessage.setText("<div>" +
-                "<h1>Tebrikler</h1>" +
-                "<div>" +
-                icerik +
-                //Ürün 275 tlye emirhan karakoc kişisine satılmıştır. Hadi hayırlı olsun.
-                "</div>" +
-                "</div>");
-        simpleMailMessage.setSubject("Müzayede kazandınız.!");
-        mailSender.send(simpleMailMessage);
-
-
-
-        return "Gonderildi";
+        try {
+            helper.setFrom("shopifyemirhan6@gmail.com");
+            helper.setTo(to);
+            helper.setSubject("Müzayede kazandınız.!");
+            String htmlContent = "<div>" +
+                    "<h1>Tebrikler</h1>" +
+                    "<div>" +
+                    icerik +
+                    "</div>" +
+                    "</div>";
+            helper.setText(htmlContent, true); // true: HTML içerik olduğunu belirtir
+            mailSender.send(mimeMessage);
+            return "Gönderildi";
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return "Hata: " + e.getMessage();
+        }
     }
-
-
 }
