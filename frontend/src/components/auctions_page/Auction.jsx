8b5694fa998433703a80dc7f3375d59c3ff2e118
EMİRHAN KARAKOÇ <emirhankarakoc@yahoo.com>
2024-03-28 17:10:59 +0300

    soccket disconnect silindi.

--- frontend/src/components/auctions_page/Auction.jsx
+++ frontend/src/components/auctions_page/Auction.jsx
@@ -6,12 +6,12 @@ import { http, httpError } from "../../lib/http";
 import OffersTable from "./OffersTable";
 import io from "socket.io-client";
 
-export default function Auction(props) {
+export default function Auction() {
   const { id } = useParams();
 
   const [auction, setAuction] = useState(null);
   const [newOffer, setNewOffer] = useState(false);
-  const [isLoad, setIsLoad] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     //const socket = io("ws://ws.backend.kgzkbi.easypanel.host/", {
@@ -22,91 +22,103 @@ export default function Auction(props) {
     });
 
     socket.on("new_offer", () => {
-      console.log("teklif geldi!");
-      setNewOffer(!newOffer);
+      console.log("Yeni teklif geldi!");
+      setNewOffer((prevState) => !prevState);
     });
 
-    const fetchProduct = async () => {
+    const fetchAuction = async () => {
       try {
-        setIsLoad(true);
         const response = await http.get(`/auctions/${id}`);
-        setIsLoad(false);
         setAuction(response.data);
       } catch (error) {
         console.log(httpError(error));
+      } finally {
+        setIsLoading(false);
       }
     };
-    fetchProduct();
+
+    fetchAuction();
+
+    // Cleanup
     return () => {
-      socket.disconnect();
+      socket.off("new_offer");
     };
-  }, []);
+  }, [id]);
+
   const createOffer = async () => {
     if (!localStorage.getItem("userToken")) {
       window.location.replace("/login");
+      return;
     }
+
     try {
       const form = new FormData();
       form.append("price", auction.startPrice + 50);
       form.append("auctionId", auction.id);
       form.append("userToken", localStorage.getItem("userToken"));
 
-      const data = await http.post("/offers", form);
-      setNewOffer(!newOffer); // Teklif yapıldığında OffersTable yenilensin
+      await http.post("/offers", form);
+      setNewOffer(!newOffer);
+      setAuction((prevAuction) => ({
+        ...prevAuction,
+        startPrice: prevAuction.startPrice + 50,
+      }));
+      console.log("Yeni teklif gönderildi.");
     } catch (error) {
       console.log(httpError(error));
       window.location.replace(`/auction/${auction.id}`);
-    } finally {
-      setAuction({ startPrice: auction.startPrice + 50 });
     }
-    console.log("yeni teklif gonderdim.");
   };
-  if (isLoad)
+
+  if (isLoading)
     return (
-      <>
-        <div>Yüklenirken lütfen bekleyiniz.</div>
+      <div className="bg-dark">
+        <Navbar />
+        <div className="text-light">Yüklenirken lütfen bekleyiniz.</div>
         <Spinner size="xl" color="white" />
-      </>
+      </div>
     );
-  if (!auction) {
-    return <div>Gösterilecek bir müzayede yok.</div>;
-  }
+
+  if (!auction)
+    return (
+      <div className="bg-dark">
+        <Navbar />
+        <div className="text-light">Gösterilecek bir müzayede yok.</div>
+      </div>
+    );
+
   return (
     <div className="bg-dark">
       <Navbar />
-
-      {auction && (
-        <Container>
-          <Row className="my-3">
-            <Col>
-              <div>
-                <h2 className="text-light d-flex float-left">
-                  {auction.product.productTitle}
-                </h2>
-                <img
-                  style={{ width: "700px", height: "300px" }}
-                  src={auction.product.photoPath}
-                ></img>
-                <h4 className="text-light my-2">
-                  FIYAT: {auction.startPrice} TL
-                </h4>
-              </div>
-              <Button className="bg-success" onClick={createOffer}>
-                {auction.startPrice + 50} TL TEKLIF YAP
-              </Button>
-            </Col>
-            <Col className="text-white">
-              <OffersTable key={newOffer} id={auction.id} />
-            </Col>
-          </Row>
-
-          <h3 className="text-light d-flex float-left">ÜRÜN AÇIKLAMASI</h3>
-          <div className="border border-success text-white d-flex float-left  p-2 my-3">
-            {auction.description}
-          </div>
-        </Container>
-      )}
-
+      <Container>
+        <Row className="my-3">
+          <Col>
+            <div>
+              <h2 className="text-light d-flex float-left">
+                {auction.product.productTitle}
+              </h2>
+              <img
+                style={{ width: "700px", height: "300px" }}
+                src={auction.product.photoPath}
+                alt="Ürün Fotoğrafı"
+              />
+              <h4 className="text-light my-2">
+                FIYAT: {auction.startPrice} TL
+              </h4>
+            </div>
+            <Button className="bg-success" onClick={createOffer}>
+              {auction.startPrice + 50} TL TEKLIF YAP
+            </Button>
+          </Col>
+          <Col className="text-white">
+            <OffersTable key={newOffer} id={auction.id} />
+          </Col>
+        </Row>
+        <h3 className="text-light d-flex float-left">ÜRÜN AÇIKLAMASI</h3>
+        <div className="border border-success text-white d-flex float-left  p-2 my-3">
+          {auction.description}
+        </div>
+      </Container>
       <Link to={"/auctions"} className="text-white text-decoration-none">
         <Button>Geri Dön</Button>
       </Link>
